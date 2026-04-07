7d043d02a25e1a64cec4311e7b3068d3e2b41e1f
min zhang <zhangmin1984121@gmail.com>
2026-04-07 15:18:48 -0700

    example7 added

---
+++ example7/main.c
@@ -0,0 +1,13 @@
+#include "mathlib.h"
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    argc = argc; /* silence unused parameter warning */ 
+    argv = argv; /* silence unused parameter warning */
+    int a = 10, b = 5;
+    printf("add: %d\n", add(a, b));
+    printf("sub: %d\n", sub(a, b));
+    printf("mul: %d\n", mul(a, b));
+    printf("div: %d\n", div(a, b));
+    return 0;
+}
